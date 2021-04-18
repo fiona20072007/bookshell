d1dd81c6984709fb3fed0ac35de3a60bd499f8e5
Fiona <fiona20072007@gmail.com>
2021-04-18 09:37:19 +0800

    add function for adding style to picked card

--- src/components/BookList/index.vue
+++ src/components/BookList/index.vue
@@ -1,51 +1,49 @@
 <template>
   <div>
     <router-link :to="{ name: 'BookDetail', params: { bookId: book.id }}" v-for="book in bookList" :key="book.id">
-      <div>
-        <img :src="book.image" :alt="book.name"/>
+      <div :class="{ activeBook: bookId === book.id }">
+        <div>
+          <img :src="book.image" :alt="book.name"/>
+        </div>
+        <div>{{ book.name }}{{book.id}}</div>
       </div>
-      <div>{{ book.name }}</div>
     </router-link>
     <router-view/>
   </div>
 </template>
 
 <script>
-import { getData } from "../../util"
+import {getData} from "../../util"
 import {getBooksDetailUrl} from "../../constants"
 
 export default {
   data() {
     return {
-      bookList: ''
+      bookList: '',
+      bookId: ''
     }
+  },
+  created() {
+    this.getBookId()
+  },
+  watch: {
+    '$route': 'getBookId'
   },
   mounted() {
     getData(getBooksDetailUrl).then(response => (this.bookList = response.data))
         .catch(function (error) {
           console.log(error)
         });
+  },
+  methods: {
+    getBookId() {
+      return this.bookId = parseInt(this.$route.params.bookId)
+    }
   }
 }
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
-<style scoped>
-h3 {
-  margin: 40px 0 0;
-}
-
-ul {
-  list-style-type: none;
-  padding: 0;
-}
-
-li {
-  display: inline-block;
-  margin: 0 10px;
-}
-
-a {
-  color: #42b983;
-}
+<style scoped lang="scss">
+@import "../.././assets/scss/booklist";
 </style>
